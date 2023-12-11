f8195d501faec25ee3ced80b0ca2b03a2d401094
kobbin003 <dkfeto@gmail.com>
2023-12-11 11:10:51 +0530

    dist and node_modules removed

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,10 +1,3 @@
-import express from "express";
-const app = express();
-let PORT = process.env.PORT || 3000;
-// if (process.env.NODE_ENV == "production") {
-// }
-app.get("/", (req, res) => {
-    res.json({ msg: "Hello World!!!" });
-});
-app.listen(PORT, () => console.log(`app is running in port: ${PORT}`));
+console.log("hello trial");
+export {};
 //# sourceMappingURL=index.js.map
