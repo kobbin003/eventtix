92f246c2a11f4c218dfea4ed0186c36f5f7de890
kobbin003 <dkfeto@gmail.com>
2023-12-11 20:51:12 +0530

    prisma schema added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
-
+import { PrismaClient } from "@prisma/client";
+import { accountRouter } from "./routes/accountRoute.js";
+import { notFound } from "./utils/notFound.js";
+import { errorHandler } from "./utils/errorHandler.js";
+import passport from "passport";
+import { initializePassportWithJwtStrategy } from "./passport/jwtstrategy.js";
 const app = express();
 
 let PORT = process.env.PORT || 3000;
 
-// if (process.env.NODE_ENV == "production") {
-// }
+initializePassportWithJwtStrategy();
+
+export const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 	res.json({ msg: "HOME" });
@@ -14,5 +24,26 @@ app.get("/", (req, res) => {
 app.get("/hello", (req, res) => {
 	res.json({ msg: "Hello World!!!" });
 });
+
+app.get("/check", async (req, res) => {
+	try {
+		const example = await prisma.example.create({
+			data: { name: "kobin", age: 31 },
+		});
+		res.json(example);
+	} catch (error) {
+		res.status(400).send("Error");
+	}
+});
+
+// app.use("/account", accountRouter);
+
+/** 404 route handling middleware */
+app.use((req, res, next) => {
+	notFound(req, res, next);
+});
+
+/** error handler */
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`app is running in port: ${PORT}`));
