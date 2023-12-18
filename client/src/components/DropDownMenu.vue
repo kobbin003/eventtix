31d4d6fb88f0f54763664392c891b9c8eb417721
kobbin003 <dkfeto@gmail.com>
2023-12-18 20:36:23 +0530

    dropdown menu

---
+++ client/src/components/DropDownMenu.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<div class="dropdown dropdown-end">
+			<div
+				tabindex="0"
+				role="button"
+				class="btn btn-sm rounded-sm m-1 border border-white font-normal"
+			>
+				account
+			</div>
+			<ul
+				tabindex="0"
+				class="dropdown-content z-[1] menu shadow bg-base-100 w-max rounded-sm border border-gray-100/5"
+			>
+				<li>
+					<RouterLink
+						to="/user/dashboard/events"
+						class="rounded-sm bg-transparent shadow-none border-none"
+						><span class="">your events</span></RouterLink
+					>
+				</li>
+				<li>
+					<RouterLink
+						to="/user/dashboard"
+						class="rounded-sm bg-transparent shadow-none border-none"
+						><span class="">Profile</span></RouterLink
+					>
+				</li>
+				<li>
+					<RouterLink
+						to="/login"
+						class="rounded-sm bg-transparent shadow-none border-none"
+						><span class="">Logout</span></RouterLink
+					>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
